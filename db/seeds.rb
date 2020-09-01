2fc11a071fbd015d702fbf157a831ca67b2f493a
Shawn Kearney <shawnpk29@me.com>
2020-08-31 18:58:38 -0600

    Seed progress_note data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+ProgressNote.create content: "/sh/ IWP imitative -++--+++-+ <strong><em>6/10 60%</em></strong><br>errors: sheep, she, shape"
+ProgressNote.create content: "<strong>Expressive/Pragmatic Language - book (the tiny seed)</strong><br>STG2: named vocabulary in pictures -visual/verbal/max cues<br>sun -+<br>fish +<br>seed +<br>bird --<br>grass +<br>snow -<br>rain -<br>foot -<br>flower --<br>tree +<br><strong><em>Total: 5/13=38%</em></strong>"
+ProgressNote.create content: "STG3: labeled vocabulary (nouns, verbs) -visual/mod cues:<br>nouns: +++++ +++ 8/8<br>errors: 0<br>verbs: ++ - - - - + 3/7<br>errors: swim, kick, catch, throw<br><strong><em>Total: 10/13=77%</em></strong>"
+
+puts "Created #{ProgressNote.count} progress notes"
